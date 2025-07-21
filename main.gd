66dd0f3f320222b4c9be8d8ab94cc85795333e42
Frederic Vanmol <red.job5989@axiworld.be>
2025-07-21 22:04:32 +0200

    #151 Added a "connecting" screen

--- main.gd
+++ main.gd
@@ -6,13 +6,12 @@ var Client: TClient
 
 
 func _ready() -> void:
-	Server = TServer.new()
-	add_child(Server)
-	Client = TClient.new()
-	add_child(Client)
+	_InitializeServer()
+	_InitializeClient()
 	
 	$Menu.Initialize(Server, Client)
 	$Lobby.Initialize(Server, Client)
+	$Connecting.Initialize(Server, Client)
 	
 	Tools.SwitchToScene($Menu)
 	pass
@@ -22,6 +21,31 @@ func _on_lobby_on_leave_lobby() -> void:
 	Tools.SwitchToScene($Menu)
 	pass
 
-func _on_menu_on_switch_to_lobby() -> void:
+func _InitializeServer() -> void:
+	Server = TServer.new()
+	add_child(Server)
+	pass
+
+func _InitializeClient() -> void:
+	Client = TClient.new()
+	add_child(Client)
+	
+	Client.OnConnectedToServer.connect(_client_connected_to_server)
+	Client.OnConnectionToServerFailed.connect(_client_connection_to_server_failed)
+	pass
+
+
+func _client_connected_to_server() -> void:
 	Tools.SwitchToScene($Lobby)
+	pass
+
+
+func _client_connection_to_server_failed() -> void:
+	Tools.ShowAlert("Failed to connect")
+	Tools.SwitchToScene($Menu)
+	pass
+
+
+func _on_menu_on_connecting() -> void:
+	Tools.SwitchToScene($Connecting)
 	pass
--- ui/menu.gd
+++ ui/menu.gd
@@ -1,18 +1,16 @@
 extends TScene
 
 
-signal OnSwitchToLobby
+signal OnConnecting
 
 
 func BeforeShow() -> void:
 	super()
-	Client.OnConnectedToServer.connect(_client_connected_to_server)
 	Client.OnConnectionToServerFailed.connect(_client_connection_to_server_failed)
 	pass
 
 func AfterHide() -> void:
 	super()
-	Client.OnConnectedToServer.disconnect(_client_connected_to_server)
 	Client.OnConnectionToServerFailed.disconnect(_client_connection_to_server_failed)
 	pass
 
@@ -33,19 +31,15 @@ func _HandleUserInput():
 func _on_host_btn_pressed() -> void:
 	if Server.Start():
 		if Client.Connect("127.0.0.1"):
-			OnSwitchToLobby.emit()
 			return
 	Tools.ShowAlert("Something went wrong when starting the server")
 	pass
 
 func _on_join_btn_pressed() -> void:
 	Client.Connect("127.0.0.1")
+	OnConnecting.emit()
 	pass
 
-
-func _client_connected_to_server() -> void:
-	OnSwitchToLobby.emit()
-	pass
 
 func _client_connection_to_server_failed() -> void:
 	Tools.ShowAlert("Failed to connect")
