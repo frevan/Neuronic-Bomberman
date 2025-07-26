f43a317e737e5a69240333ac63f21ddb80261d93
Frederic Vanmol <red.job5989@axiworld.be>
2025-07-26 14:40:00 +0200

    #179 More dynamic text on the score screen

--- ui/score.gd
+++ ui/score.gd
@@ -28,3 +28,24 @@ func _ContinueToMatchOrLobby() -> void:
 		OnLeaveMatch.emit()
 	elif Client.State == Client.TState.MATCH:
 		OnReadyForNextRound.emit()
+	pass
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		_UpdateMessageLabel()
+	pass
+
+
+func _UpdateMessageLabel():
+	match Client.State:
+		Client.TState.LOBBY:
+			var idx = Client.Data.FindSlotForPlayer(Network.PeerID)
+			if idx != Types.INVALID_SLOT:
+				if Client.Data.Slots[idx].Alive:
+					$MessageLabel.text = "You won!"
+				else:
+					$MessageLabel.text = "You lost!"
+		Client.TState.MATCH:
+			$MessageLabel.text = "On to the next round..."
+	pass
