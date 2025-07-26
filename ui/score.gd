b0bad09a01c42a72f9810da102480aff9a6d11ef
Frederic Vanmol <red.job5989@axiworld.be>
2025-07-26 14:48:24 +0200

    #181 Show when the players tied for the win

--- ui/score.gd
+++ ui/score.gd
@@ -37,15 +37,36 @@ func _on_visibility_changed() -> void:
 	pass
 
 
+func _IsThereATieForTheWin(HighestScore: int) -> bool:
+	var count: int = 0
+	for slot: Types.TSlot in Client.Data.Slots:
+		if slot.Score == HighestScore:
+			count += 1
+	return count > 1
+
+
 func _UpdateMessageLabel():
+	var highest_score: int = 0
+	var my_score: int = 0
+	for slot: Types.TSlot in Client.Data.Slots:
+		highest_score = max(slot.Score, highest_score)
+		if slot.PlayerID == Network.PeerID:
+			my_score = slot.Score
+	var tied: bool = _IsThereATieForTheWin(highest_score)
+	
+	var msg: String = "We're done here"	
 	match Client.State:
 		Client.TState.LOBBY:
 			var idx = Client.Data.FindSlotForPlayer(Network.PeerID)
 			if idx != Types.INVALID_SLOT:
-				if Client.Data.Slots[idx].Alive:
-					$MessageLabel.text = "You won!"
+				if my_score == highest_score:
+					if tied:
+						msg = "You tied for the win!"
+					else:
+						msg = "You won!"
 				else:
-					$MessageLabel.text = "You lost!"
+					msg = "You lost!"
 		Client.TState.MATCH:
-			$MessageLabel.text = "On to the next round..."
+			msg = "On to the next round..."
+	$MessageLabel.text = msg
 	pass
