7211f9694142141f79b55844b8899b89441ee2de
Frederic Vanmol <red.job5989@axiworld.be>
2025-09-11 18:37:26 +0200

    #250 Use joystick to move around

--- ui/items/player_input.gd
+++ ui/items/player_input.gd
@@ -10,8 +10,22 @@ signal OnDirectionChanged(NewDirection: Vector2)
 func _process(_delta: float) -> void:
 	if !get_parent().visible:
 		return
-	var old_direction = direction
-	direction = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+	var old_direction: Vector2 = direction
+	var new_direction: Vector2 = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+	direction = _LimitDirection(new_direction)
 	if old_direction != direction:
 		OnDirectionChanged.emit(direction)
 	pass
+
+
+func _LimitDirection(Value: Vector2) -> Vector2:
+	if Value != Vector2.ZERO:
+		if absf(Value.x) < 0.5:
+			Value.x = 0.0
+		if absf(Value.y) < 0.5:
+			Value.y = 0.0
+		if absf(Value.x) >= absf(Value.y):
+			return Vector2(1.0 * signf(Value.x), 0.0)
+		else:
+			return Vector2(0.0, 1.0 * signf(Value.y))
+	return Value
